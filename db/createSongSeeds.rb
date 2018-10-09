1b02e81caad0b6274170379aaf0a96524e95d6f5
Joseph Olivier <josepholivier92@gmail.com>
2018-10-08 22:57:53 -0700

    Change songSeeds to include album art

--- db/createSongSeeds.rb
+++ db/createSongSeeds.rb
@@ -11,12 +11,13 @@ def generateSongsArtistsAndAlbums
   CSV.foreach("./raw_tracks.csv") do |row|
     unless (row[0].to_i == 0 || row[4] == nil || row[6] == nil || row[8] == nil)
       if songIds.include?(row[0].to_i)
-        song = {:name => "", :artist => "", :album => "", :genre => "", :song_id => 0}
+        song = {:song_id => 0, :name => "", :artist => "", :album => "", :genre => "", :album_art_url => ""}
         song[:name] = row[8]
         song[:artist] = row[4]
         song[:album] = row[2]
         song[:song_id] = row[0].to_i
         song[:genre] = row[6].split("genre_title").join("").split("'")[7]
+        song[:album_art_url] = row[7]
         songArr.push(song)
       end
     end
